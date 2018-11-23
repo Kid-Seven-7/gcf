6c56c39edef991adc55caf46b4604e0a3b74829c
afrika matshiye <amatshiy@student.42.fr>
2018-11-23 10:57:19 +0200

    Encryption added to password for login

--- lib/login.dart
+++ lib/login.dart
@@ -1,35 +1,44 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+// import 'dart:core';
+import 'package:dbcrypt/dbcrypt.dart';
 
 class LoginPage extends StatefulWidget {
   @override
   State createState() => new LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
-
+class LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   AnimationController _iconAnimationController;
   Animation<double> _iconAnimation;
+  final textName = new TextEditingController();
+  final textPassword = new TextEditingController();
+  DBCrypt dBCrypt = DBCrypt();
 
   @override
-  void initState(){
+  void dispose() {
+    textName.dispose();
+    textPassword.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
     super.initState();
     _iconAnimationController = new AnimationController(
-      vsync: this,
-      duration: new Duration(milliseconds: 1000)
-    );
+        vsync: this, duration: new Duration(milliseconds: 1000));
 
     _iconAnimation = new CurvedAnimation(
-      parent:_iconAnimationController,
+      parent: _iconAnimationController,
       curve: Curves.easeIn,
     );
-    _iconAnimation.addListener(()=>this .setState((){}));
+    _iconAnimation.addListener(() => this.setState(() {}));
     _iconAnimationController.forward();
   }
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
       body: Stack(
@@ -46,62 +55,64 @@ class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixi
             children: <Widget>[
               new Image(
                 image: new AssetImage("assets/images/GCF-logo.png"),
-                height: _iconAnimation.value *100,
-                width: _iconAnimation.value *100,
+                height: _iconAnimation.value * 100,
+                width: _iconAnimation.value * 100,
               ),
-
-
               new Form(
-                child: Theme (
+                child: Theme(
                   data: ThemeData(
-                    brightness: Brightness.dark, 
+                    brightness: Brightness.dark,
                     primarySwatch: Colors.green,
                     inputDecorationTheme: new InputDecorationTheme(
-                      labelStyle: new TextStyle(
-                        color: Color.fromARGB(255, 140, 188, 63),
-                        fontSize: 20.0,
-                      )
-                    ),
+                        labelStyle: new TextStyle(
+                      color: Color.fromARGB(255, 140, 188, 63),
+                      fontSize: 20.0,
+                    )),
                   ),
                   child: Container(
                     padding: const EdgeInsets.all(40.0),
                     child: new Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                     new TextFormField(
-                      decoration: new InputDecoration(
-                        labelText: "Name"
-                      ),
-                      keyboardType: TextInputType.text,
-                    ),
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: new InputDecoration(labelText: "Name"),
+                          keyboardType: TextInputType.text,
+                          controller: textName,
+                        ),
+                        new TextFormField(
+                          decoration:
+                              new InputDecoration(labelText: "Password"),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          controller: textPassword,
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                        ),
+                        new MaterialButton(
+                          color: Color.fromARGB(255, 140, 188, 63),
+                          child: new Text("Login"),
+                          onPressed: () {
+                            print(textName.text);
 
-                    new TextFormField(
-                      decoration: new InputDecoration(
-                        labelText: "Password"
-                      ),
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                    ),
-                    new Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                    ),
+                            var hash = dBCrypt.hashpw(
+                                textPassword.text, dBCrypt.gensalt());
 
-                    new MaterialButton(
-                      color: Color.fromARGB(255, 140, 188, 63),
-                      child: new Text("Login"),
-                      onPressed: () {
-                        Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => FirstScreen()),
-                        );
-                      },
-                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(70.0)),
-                      splashColor: Colors.white,
+                            print(hash);
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //       builder: (context) => FirstScreen()),
+                            // );
+                          },
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(70.0)),
+                          splashColor: Colors.white,
+                        ),
+                      ],
                     ),
-                    ],
-              ),
                   ),
                 ),
-                 
               )
             ],
           )
