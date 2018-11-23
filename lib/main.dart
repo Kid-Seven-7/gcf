b5a1b5b2b737066a2946260d2dcef73b3f72877f
ColdHeat <gsebaetse@gmail.com>
2018-11-23 17:20:11 +0200

    Merge branch 'gsebaets'
    po

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-//packages
 import 'package:flutter/material.dart';
 
-//source files
-import 'home_page.dart';
-
 void main() {
+<<<<<<< HEAD
   runApp(MaterialApp(
     title: 'Navigation Basics',
     home: HomeScreen(),
     debugShowCheckedModeBanner: false,
   ));
 }
+=======
