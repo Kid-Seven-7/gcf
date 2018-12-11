ed754c292335d7b1d88742cf483687e2744b3c5c
Joseph NGOMA <jngoma@e5r6p4.wethinkcode.co.za>
2018-12-11 16:35:11 +0200

    basic log page

---
+++ lib/frontend/log_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'package:gcf_projects_app/backend/globals.dart';
+import 'project_file_card.dart';
+
+class LogPage extends StatefulWidget {
+  @override
+  _LogPageState createState() => new _LogPageState();
+}
+
+class _LogPageState extends State<LogPage> {
+  GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
+  _handleDrawer() {
+    _key.currentState.openDrawer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        key: _key,
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 140, 188, 63),
+          title: Text("Log Page"),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: _handleDrawer,
+          ),
+          actions: <Widget>[
+            Image.asset('assets/images/gcf_white.png'),
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            LogItem()
+          ],
+        ) ,
+//        bottomNavigationBar: NavBar(),
+//        drawer: OpenDrawer()
+    );
+  }
+}
+
+ListTile OpenLog(){
+  var item = LogItem();
+}
+
+class LogItem extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.assignment),
+      title: const Text('The for the narrator'),
+    );
+  }
+}
