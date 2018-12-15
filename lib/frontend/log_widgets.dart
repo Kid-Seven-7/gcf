8390ae6038069cebfe4902d566867b4b132d6b02
jngoma <joseph.ngoma@moov.life>
2018-12-15 23:43:02 +0200

    Created log Widget file for custom widget information

---
+++ lib/frontend/log_widgets.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+import 'log_page.dart';
+
+Widget generalInfo(BuildContext context, Log log) {
+  return   Container(
+      child: Card(
+        child:
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.library_books),
+              title: Text(
+                "Project Name",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectName),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.assignment),
+              title: Text(
+                "Description",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectDescription),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text(
+                "Foreman on Site",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectForeman),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.access_time),
+              title: Text(
+                "Time taken",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("Some value"),
+            ),
+          ],
+        ),
+      )
+  );
+}
+
+Widget budgetInfo(BuildContext context, Log log) {
+  return   Container(
+      child: Card(
+        child:
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.library_books),
+              title: Text(
+                "Project Name",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectName),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.assignment),
+              title: Text(
+                "Description",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectDescription),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text(
+                "Foreman on Site",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectForeman),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.access_time),
+              title: Text(
+                "Time taken",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("Some value"),
+            ),
+          ],
+        ),
+      )
+  );
+}
+
+Widget timeInfo(BuildContext context, Log log) {
+  return   Container(
+      child: Card(
+        child:
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.library_books),
+              title: Text(
+                "Project Name",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectName),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.assignment),
+              title: Text(
+                "Description",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectDescription),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text(
+                "Foreman on Site",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(log.projectForeman),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 140, 188, 63),
+            ),
+            ListTile(
+              leading: Icon(Icons.access_time),
+              title: Text(
+                "Time taken",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("Some value"),
+            ),
+          ],
+        ),
+      )
+  );
+}
