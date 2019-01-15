e9ad19f39c57a0036fe465201c9c6d40dfce7bfd
Joseph NGOMA <jngoma@e4r6p11.wethinkcode.co.za>
2019-01-15 14:35:38 +0200

    Altered Burger menu

--- lib/frontend/burger_menu_drawer.dart
+++ lib/frontend/burger_menu_drawer.dart
@@ -14,15 +14,23 @@ import 'package:gcf_projects_app/backend/globals.dart';
 final storage = new FlutterSecureStorage();
 Firestore firestore = new Firestore();
 
-void _handleDrawer() {
-  GlobalKey<ScaffoldState> key = new GlobalKey<ScaffoldState>();
-  key.currentState.openDrawer();
-}
+//void _handleDrawer() {
+//  GlobalKey<ScaffoldState> key = new GlobalKey<ScaffoldState>();
+//  key.currentState.openDrawer();
+//}
 
+/*
+  Parameter:
+
+  Function:
+
+  Return:
+
+*/
 class OpenDrawer extends StatelessWidget {
   final TextStyle _navMenuText = TextStyle(fontSize: 18.0, color: Colors.black);
   final TextStyle _navMenuHeading =
-      TextStyle(fontSize: 25.0, color: Colors.white);
+  TextStyle(fontSize: 25.0, color: Colors.white);
 
   @override
   Widget build(BuildContext context) {
@@ -45,136 +53,147 @@ class OpenDrawer extends StatelessWidget {
 
     return new Drawer(
         child: ListView(
-      padding: EdgeInsets.zero,
-      children: <Widget>[
-        DrawerHeader(
-          padding: EdgeInsets.all(50.0),
-          child: Column(
-            children: <Widget>[
-              Row(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              padding: EdgeInsets.all(50.0),
+              child: Column(
                 children: <Widget>[
-                  Icon(
-                    Icons.account_circle,
-                    color: Colors.white,
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.account_circle,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        userName,
+                        style: _navMenuHeading,
+                      ),
+                    ],
                   ),
                   Text(
-                    userName,
-                    style: _navMenuHeading,
-                  ),
+                    'Title: $roleStatus',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
                 ],
               ),
-              Text(
-                'Title: $roleStatus',
-                style: TextStyle(
-                  color: Colors.white,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: new AssetImage("assets/images/burger_back.png"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
                 ),
-              )
-            ],
-          ),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: new AssetImage("assets/images/burger_back.png"),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+                color: Colors.blueGrey.shade900,
+                backgroundBlendMode: BlendMode.darken,
+              ),
             ),
-            color: Colors.blueGrey.shade900,
-            backgroundBlendMode: BlendMode.darken,
-          ),
-        ),
-        ListTile(
-          leading: Icon(Icons.account_circle),
-          title: Text(
-            'Manage Users',
-            style: _navMenuText,
-          ),
-          onTap: () {
-            openpage(context, "Manage Users");
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.library_books),
-          title: Text(
-            'Projects',
-            style: _navMenuText,
-          ),
-          onTap: () {
-            openpage(context, "Projects");
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.assignment),
-          title: Text(
-            'Log',
-            style: _navMenuText,
-          ),
-          onTap: () {
-            openpage(context, "Log");
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.timeline),
-          title: Text(
-            'Statistics',
-            style: _navMenuText,
-          ),
-          onTap: () {
-            openpage(context, "Stats");
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.rate_review),
-          title: Text(
-            'Reports',
-            style: _navMenuText,
-          ),
-          onTap: () {},
-        ),
-        ListTile(
-          leading: Icon(Icons.notifications),
-          title: (notifications > 0)
-              ? Text(
-                  'Notifications($notifications)',
-                  style: TextStyle(fontSize: 18.0, color: Colors.redAccent),
-                )
-              : Text(
-                  'Notifications',
-                  style: _navMenuText,
-                ),
-          onTap: () {
-            openpage(context, "Notifications");
-          },
-        ),
-        Divider(
-          height: 20.0,
-          color: Colors.black,
-        ),
-        ListTile(
-          leading: Icon(Icons.settings),
-          title: Text(
-            'Settings',
-            style: _navMenuText,
-          ),
-          onTap: () {},
-        ),
-        ListTile(
-          leading: Icon(Icons.power),
-          title: Text(
-            'Log Out',
-            style: _navMenuText,
-          ),
-          onTap: () async {
-            await storage.delete(key: "name").catchError((onError){});
-            await storage.delete(key: "role").catchError((onError){});
-            await storage.delete(key: "rememberMe").catchError((onError){});
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(
+                'Manage Users',
+                style: _navMenuText,
+              ),
+              onTap: () {
+                openpage(context, "Manage Users");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.library_books),
+              title: Text(
+                'Projects',
+                style: _navMenuText,
+              ),
+              onTap: () {
+                openpage(context, "Projects");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.assignment),
+              title: Text(
+                'Log',
+                style: _navMenuText,
+              ),
+              onTap: () {
+                openpage(context, "Log");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.timeline),
+              title: Text(
+                'Statistics',
+                style: _navMenuText,
+              ),
+              onTap: () {
+                openpage(context, "Stats");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.rate_review),
+              title: Text(
+                'Reports',
+                style: _navMenuText,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: (notifications > 0)
+                  ? Text(
+                'Notifications($notifications)',
+                style: TextStyle(fontSize: 18.0, color: Colors.redAccent),
+              )
+                  : Text(
+                'Notifications',
+                style: _navMenuText,
+              ),
+              onTap: () {
+                openpage(context, "Notifications");
+              },
+            ),
+            Divider(
+              height: 20.0,
+              color: Colors.black,
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                'Settings',
+                style: _navMenuText,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text(
+                'Log Out',
+                style: _navMenuText,
+              ),
+              onTap: () async {
+                await storage.delete(key: "_number").catchError((onError){});
+                await storage.delete(key: "password").catchError((onError){});
+                await storage.delete(key: "firstRun");
+                await storage.delete(key: "name").catchError((onError){});
+                await storage.delete(key: "role").catchError((onError){});
+                await storage.delete(key: "rememberMe").catchError((onError){});
 
-            openpage(context, "LogOut");
-          },
-        ),
-      ],
-    ));
+                openpage(context, "LogOut");
+              },
+            ),
+          ],
+        ));
   }
 }
 
+/*
+  Parameter:
+
+  Function:
+
+  Return:
+
+*/
 void openpage(BuildContext context, String page) {
   if (page == "Manage Users") {
     if (currentPage != "Manage Users") {
